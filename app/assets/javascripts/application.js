78eae768f123c6e875fa4db50675176568d2d87e
Chris Hobbs <chobbs1755@icloud.com>
2016-01-21 21:56:21 -0500

    Got Masonry working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require masonry/jquery.masonry
-//= require masonry/jquery.imagesloaded.min
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
